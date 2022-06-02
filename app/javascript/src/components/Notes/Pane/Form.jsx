bd6a1feb7a326d6decd3cb125e563b978ddfea0c
udai1931 <udaigupta19311@gmail.com>
2022-06-02 15:22:35 +0530

    Added Note form, closes issue #5

---
+++ app/javascript/src/components/Notes/Pane/Form.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+import { Formik, Form } from "formik";
+import { Button, Pane } from "neetoui";
+import { Toastr } from "neetoui";
+import { Input, Textarea } from "neetoui/formik";
+
+import { NOTES_FORM_VALIDATION_SCHEMA } from "../constants";
+
+export default function NoteForm({
+  onClose,
+  // refetch,
+  note,
+}) {
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    try {
+      // await notesApi.create(values);
+      // refetch();
+      onClose();
+      Toastr.success("Form has been successfully saved.");
+    } catch (err) {
+      // logger.error(err);
+    }
+  };
+
+  return (
+    <Formik
+      initialValues={note}
+      onSubmit={handleSubmit}
+      validateOnBlur={submitted}
+      validateOnChange={submitted}
+      validationSchema={NOTES_FORM_VALIDATION_SCHEMA}
+    >
+      {({ isSubmitting }) => (
+        <Form className="w-full">
+          <Pane.Body className="space-y-6">
+            <Input
+              label="Title"
+              name="title"
+              className="w-full flex-grow-0"
+              required
+            />
+            <Textarea
+              label="Description"
+              name="description"
+              className="w-full flex-grow-0"
+              rows={8}
+              required
+            />
+          </Pane.Body>
+          <Pane.Footer>
+            <Button
+              type="submit"
+              label="Save Changes"
+              size="large"
+              style="primary"
+              className="mr-3"
+              disabled={isSubmitting}
+              loading={isSubmitting}
+              onClick={() => setSubmitted(true)}
+            />
+            <Button
+              onClick={onClose}
+              label="Cancel"
+              size="large"
+              style="text"
+            />
+          </Pane.Footer>
+        </Form>
+      )}
+    </Formik>
+  );
+}
